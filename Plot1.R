25d6bde4779518ef41bfc17a438faac2a40c4b7b
Ekhtiar Syed <ekhtiar.syed@gmail.com>
2015-09-12 23:29:56 +0200

    plot1.R working

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+#first we read  the entire dataset and store it in pwdata, we are using sep=";" as data is seperated by ;
+pwdata <- read.csv(file="./data/household_power_consumption.txt", header = TRUE, sep = ";", as.is = TRUE)
+#the data is cleaned a little by converting date character to date object
+pwdata$Date <- as.Date(pwdata$Date, "%d/%m/20%y")
+#the data is subsetted to the just the days we want to analyse
+selpwdata <- subset(pwdata, (pwdata$Date >= as.Date("2007-02-01")) &(pwdata$Date <= as.Date("2007-02-02")))
+#date and time is joined together in the Time column so the entire column can be converted into a timestamp
+selpwdata$Time <- paste (selpwdata$Date, selpwdata$Time, sep = " ")
+#conversion of the Time column to a date time object 
+selpwdata$Time <- strptime(selpwdata$Time, "%Y-%m-%d %H:%M:%S")
+#change Global Active Power to numeric
+selpwdata$Global_active_power <- as.numeric(selpwdata$Global_active_power)
+#defining the png output
+png(filename = "./Plot1.png", width=480, height=480)
+#first the plot function is used to create the grid, pch= " " is used as we want to generate line grapth without points in the next step
+hist(selpwdata$Global_active_power, breaks = 6, col = "red", xlim =c(0,6), ylim = c(0,1200), main = "Global Active Power", ylab = "Frequency", xlab = "Global Active Power (kilowatt)")
+#mandatory housekeeping
+dev.off()
